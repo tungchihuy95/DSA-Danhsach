8c345d79b15b63d9a6459f626107d165359115e7
tungchihuy95 <66403401+tungchihuy95@users.noreply.github.com>
2020-07-16 15:56:57 +0700

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+
+    public static int calculate(int firstOperand, int secondOperand, char operator) {
+        switch (operator) {
+            case '+':
+                return firstOperand + secondOperand;
+            case '-':
+                return firstOperand - secondOperand;
+            case '*':
+                return firstOperand * secondOperand;
+            case '/':
+                if (secondOperand != 0)
+                    return firstOperand / secondOperand;
+                else
+                    throw new RuntimeException("Can not divide by 0");
+            default:
+                throw new RuntimeException("Unsupported operation");
+        }
+    }
+}
